60b11f72f1d1f40dff7bd17e8946610de33f61ad
Clement RICATTE <clement.ricatte57@gmail.com>
2023-01-30 15:30:14 +0100

    feat(client): :sparkles: add simple buttons for mobile responsiveness

--- client/src/components/channels/UserList.tsx
+++ client/src/components/channels/UserList.tsx
@@ -1,13 +1,33 @@
+import { faUsers } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Button from "components/Button";
 import Title from "components/Title";
 import UserItem from "components/user/UserItem";
+import { useState } from "react";
 
 export default function UserList({ users }: { users: User[] }) {
+	const [visible, setVisible] = useState(false);
+
 	return (
-		<nav className="flex flex-col w-56 h-screen bg-slate-300 dark:bg-slate-800 p-4">
-			<Title>Users - {users.length}</Title>
-			{users.map((u) => (
-				<UserItem user={u} key={u.id} className="mb-2" />
-			))}
-		</nav>
+		<>
+			<nav
+				className={`flex flex-col w-56 h-screen bg-slate-300 dark:bg-slate-800 p-4 ${
+					!visible && "max-md:hidden"
+				}`}
+			>
+				<Title>Users - {users.length}</Title>
+				{users.map((u) => (
+					<UserItem user={u} key={u.id} className="mb-2" />
+				))}
+			</nav>
+			<Button
+				className={`md:hidden absolute top-16 ${
+					visible ? "right-60" : "right-4"
+				} bg-slate-500 !aspect-square !w-10 !h-10 !text-white !rounded-full shadow-2xl hover:scale-105 transition-transform z-[1]`}
+				onClick={() => setVisible(!visible)}
+			>
+				<FontAwesomeIcon icon={faUsers} />
+			</Button>
+		</>
 	);
 }
--- client/src/components/layout/NavBar.tsx
+++ client/src/components/layout/NavBar.tsx
@@ -1,12 +1,32 @@
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Button from "components/Button";
 import NavBarChannels from "components/layout/navbar/NavBarChannels";
 import NavBarUser from "components/layout/navbar/NavBarUser";
+import { useState } from "react";
 
 export default function NavBar() {
-	return (
-		<nav className="flex flex-col w-56 h-screen bg-slate-300 dark:bg-slate-800">
-			<NavBarChannels />
+	const [visible, setVisible] = useState(false);
 
-			<NavBarUser />
-		</nav>
+	return (
+		<>
+			<nav
+				className={`flex flex-col w-56 h-screen bg-slate-300 dark:bg-slate-800 ${
+					!visible && "max-sm:hidden"
+				}`}
+			>
+				<NavBarChannels />
+
+				<NavBarUser />
+			</nav>
+			<Button
+				className={`sm:hidden absolute top-16 ${
+					visible ? "left-60" : "left-4"
+				} bg-slate-500 !aspect-square !w-10 !h-10 !text-white !rounded-full shadow-2xl hover:scale-105 transition-transform z-[1]`}
+				onClick={() => setVisible(!visible)}
+			>
+				<FontAwesomeIcon icon={faBars} />
+			</Button>
+		</>
 	);
 }
